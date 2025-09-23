926f48a35ba66709e76b4706e003de1050021ad0
Alejandro Bermúdez <mejorandro@gmail.com>
2025-09-23 14:13:37 -0600

    feat(news): Imporve looking and add dummy data

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,17 @@ import './globals.css';
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
-}
+};
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-     <MainHeader />
-      <body>{children}</body>
+      <body>
+        <div id="page">
+          <MainHeader />
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
