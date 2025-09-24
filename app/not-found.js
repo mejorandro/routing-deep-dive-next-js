2552dcda0d53f07f974d34baa18e75b5351b18e6
Alejandro Bermúdez <mejorandro@gmail.com>
2025-09-24 14:32:24 -0600

    Feat(news): Impleemented not found articles functionality and page

---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFoundPage(){
+    return <div id="error">
+            <h1>Not Found!</h1>
+            <p>The requested resource could be be found!</p>
+           </div>
+}
