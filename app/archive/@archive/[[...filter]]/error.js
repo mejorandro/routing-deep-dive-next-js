35ab8892b8feb4f4fc132e447783c5a96533f6eb
Alejandro Bermúdez <mejorandro@gmail.com>
2026-01-28 07:50:50 -0600

    (adding error page): Add error page when there is an error to avoid shwoing the raw error to the user, instead show a customized message so the user can continue using the application

---
+++ app/archive/@archive/[[...filter]]/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function FilterError(){
+    return (
+        <div id="error">
+            <h2>An error ocurred!</h2>
+            <p>Invalid Path.</p>
+        </div>
+    );
+}
